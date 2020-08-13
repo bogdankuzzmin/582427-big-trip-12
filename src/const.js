5314e63d71a24d6f77f9534d42870598b0937488
Bogdan <bogdankuzzmin@gmail.com>
2020-08-13 23:15:33 +0600

    added general constants

---
+++ src/const.js
@@ -0,0 +1,30 @@
+export const MAX_EVENT_OFFERS = 3;
+
+export const EVENT_ACTION = {
+  types: [
+    `Taxi`,
+    `Bus`,
+    `Train`,
+    `Ship`,
+    `Transport`,
+    `Drive`,
+    `Flight`,
+    `Check-in`,
+    `Sightseeing`,
+    `Restaurant`,
+  ],
+  activities: [
+    `Check-in`,
+    `Sightseeing`,
+    `Restaurant`,
+  ],
+};
+
+export const CITIES = [
+  `Hawaii`,
+  `Chicago`,
+  `Tokyo`,
+  `Valencia`,
+  `Amsterdam`,
+  `Saint-Petersburg`,
+];
