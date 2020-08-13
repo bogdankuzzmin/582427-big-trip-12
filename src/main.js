cea6dc7fa62eea9846ffee4025da55c2b9abca8d
Bogdan <bogdankuzzmin@gmail.com>
2020-08-13 23:19:15 +0600

    added fns to return objects, divided components into small pieces

--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
+import moment from "moment";
+import {separateEventsIntoDays, render} from "./utils.js";
 import {createMenuTemplate} from "./view/site-menu.js";
 import {createTripControlsTemplate} from "./view/trip-controls.js";
 import {createFilterTemplate} from "./view/filter.js";
 import {createSortTemplate} from "./view/sort.js";
 import {createNewEventTemplate} from "./view/new-event.js";
 import {createTripListTemplate} from "./view/trip-list.js";
-import {createTripWaypointTemplate} from "./view/trip-waypoint";
 
-const MAX_WAYPOINTS = 3;
+import {generateEvent} from "./mock/event.js";
+import {generateDestination} from "./mock/destination.js";
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+const MAX_WAYPOINTS = 20;
+
+export const events = new Array(MAX_WAYPOINTS).fill().map(generateEvent);
+
+export const sortedEvents = events.slice().sort((a, b) => {
+  return moment(a.startDate) - moment(b.startDate);
+});
+// console.log(sortedEvents);
+
+const destination = generateDestination();
 
 const tripMain = document.querySelector(`.trip-main`);
 
-render(tripMain, createMenuTemplate(), `afterbegin`);
+render(tripMain, createMenuTemplate(sortedEvents), `afterbegin`);
 
 const tripControls = document.querySelector(`.trip-main__trip-controls`);
 const tripControlsTitle = tripControls.querySelector(`h2`);
@@ -25,13 +34,12 @@ render(tripControls, createFilterTemplate(), `beforeend`);
 const tripEvents = document.querySelector(`.trip-events`);
 
 render(tripEvents, createSortTemplate(), `beforeend`);
-render(tripEvents, createNewEventTemplate(), `beforeend`);
+render(tripEvents, createNewEventTemplate(destination, sortedEvents[0]), `beforeend`);
 
-render(tripEvents, createTripListTemplate(), `beforeend`);
 
-const tripList = document.querySelector(`.trip-days`);
-const tripEventList = tripList.querySelector(`.trip-events__list`);
+const groupSeparatedEvents = separateEventsIntoDays(sortedEvents);
 
-for (let i = 0; i < MAX_WAYPOINTS; i++) {
-  render(tripEventList, createTripWaypointTemplate(), `beforeend`);
-}
+Object.keys(groupSeparatedEvents).forEach((oneDay, dayId) => {
+  const eventDay = (formatDate) => moment(oneDay).format(formatDate);
+  render(tripEvents, createTripListTemplate(dayId + 1, eventDay, groupSeparatedEvents[oneDay]), `beforeend`);
+});
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,15 +1,63 @@
-export const createMenuTemplate = () => {
-  return (
-    `<section class="trip-main__trip-info  trip-info">
-      <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+import moment from "moment";
+import {MAX_EVENT_OFFERS} from "../const.js";
 
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+const countPrice = (events) => {
+  let price = 0;
+
+  events.forEach((event) => {
+    price += event.price;
+
+    return event.offers
+      .slice(0, MAX_EVENT_OFFERS)
+      .filter((eventOffer) => {
+        if (eventOffer.isChecked === true) {
+          price += eventOffer.price;
+        }
+      });
+  });
+
+  return price;
+};
+
+
+const generateTripRoute = (events) => {
+  let trip = ``;
+  let lastTrip;
+
+  if (events.length > 3) {
+    trip = `${events[0].city} &mdash; . . . &mdash; ${events[events.length - 1].city}`;
+  } else {
+    for (const event of events) {
+      trip = `${trip}${event.city} — `;
+    }
+
+    lastTrip = trip.lastIndexOf(` — `);
+    trip = trip.substring(0, lastTrip);
+  }
+
+  return trip;
+};
+
+export const createMenuTemplate = (events) => {
+  const price = countPrice(events);
+  const city = generateTripRoute(events);
+
+  const firstDay = moment(events[0].startDate).format(`MMM DD`);
+  const eventsLength = events.length - 1;
+  const lastDay = moment(events[eventsLength].startDate).format(`DD`);
+
+  return (
+    `
+    <section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">${city}</h1>
+
+        <p class="trip-info__dates">${firstDay}&nbsp;&mdash;&nbsp;${lastDay}</p>
       </div>
 
       <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-    </p>
-    </section>`
-  );
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">${price}</span>
+      </p>
+    </section>
+    `);
 };
