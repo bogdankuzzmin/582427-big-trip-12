f65ad8d8949e6bc816db77c3191a7c454a5896f8
Bogdan <bogdankuzzmin@gmail.com>
2020-08-17 17:38:59 +0600

    deleted fn createEventPointsTemplate

--- src/view/trip-list.js
+++ src/view/trip-list.js
@@ -1,41 +1,37 @@
 import {createElement} from "../utils.js";
-import {createTripWayPointTemplate} from "../view/trip-waypoint.js";
+// import {createTripWayPointTemplate} from "../view/trip-waypoint.js";
 
-const createEventPointsTemplate = (events) => {
-  return events
-    .map((event) => createTripWayPointTemplate(event))
-    .join(``);
-};
+// const createEventPointsTemplate = (events) => {
+//   return events
+//     .map((event) => createTripWayPointTemplate(event))
+//     .join(``);
+// };
+// ${createEventPointsTemplate(event)}
 
-
-const createTripListTemplate = (dayId, eventDate, event) => {
-
+const createTripListTemplate = (dayId, eventDate) => {
   return (
-    `<ul class="trip-days">
-      <li class="trip-days__item  day">
+    `<li class="trip-days__item  day">
         <div class="day__info">
           <span class="day__counter">${dayId}</span>
           <time class="day__date" datetime="${eventDate(`YYYY-MM-DD`)}">${eventDate(`MMM DD`)}</time>
         </div>
 
         <ul class="trip-events__list">
-          ${createEventPointsTemplate(event)}
+
         </ul>
-      </li>
-    </ul>`
+      </li>`
   );
 };
 
 export default class TripList {
-  constructor(dayId, eventDate, event) {
+  constructor(dayId, eventDate) {
     this._dayId = dayId;
     this._eventDate = eventDate;
-    this._event = event;
     this._element = null;
   }
 
   get template() {
-    return createTripListTemplate(this._dayId, this._eventDate, this._event);
+    return createTripListTemplate(this._dayId, this._eventDate);
   }
 
   get element() {
