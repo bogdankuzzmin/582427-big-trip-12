f9cb12d9188632bb7d5a03f6ee19a4b4cc7f6850
Bogdan <bogdankuzzmin@gmail.com>
2020-08-22 23:41:19 +0600

    worked on sorting - added dataset to sort's labels - added handlers to sorting - added a check if events sorted - textContent is empty for sortDay

--- src/view/trip-sort.js
+++ src/view/trip-sort.js
@@ -1,4 +1,5 @@
 import AbstractView from "./abstract.js";
+import {SortType} from "../const.js";
 
 const createTripSortTemplate = () => {
   return (
@@ -6,13 +7,13 @@ const createTripSortTemplate = () => {
       <span class="trip-sort__item  trip-sort__item--day">Day</span>
 
       <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event">
+        <label class="trip-sort__btn" data-sort-type="${SortType.EVENT}" for="sort-event">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn" for="sort-time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" checked>
+        <label class="trip-sort__btn" data-sort-type="${SortType.TIME}" for="sort-time">
           Time
           <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
             <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
@@ -22,7 +23,7 @@ const createTripSortTemplate = () => {
 
       <div class="trip-sort__item  trip-sort__item--price">
         <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
+        <label class="trip-sort__btn" data-sort-type="${SortType.PRICE}" for="sort-price">
           Price
           <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
             <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
@@ -36,7 +37,30 @@ const createTripSortTemplate = () => {
 };
 
 export default class TripSort extends AbstractView {
+  constructor() {
+    super();
+
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   get template() {
     return createTripSortTemplate();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.matches(`.trip-sort__btn`)) {
+      const tripSortDay = evt.target.parentElement.parentElement.querySelector(`.trip-sort__item--day`);
+
+      if (tripSortDay.textContent) {
+        tripSortDay.textContent = ``;
+      }
+
+      this._callback.sortTypeChange(evt.target.dataset.sortType);
+    }
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }
