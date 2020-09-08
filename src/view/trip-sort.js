bd664edc3506c531e971afb77b8490ecb4db9bdb
Bogdan <bogdankuzzmin@gmail.com>
2020-09-08 22:26:01 +0600

    added a check for render sort DAY

--- src/view/trip-sort.js
+++ src/view/trip-sort.js
@@ -15,7 +15,7 @@ const createTripSortItemTemplate = (currentSortType) => {
 const createTripSortTemplate = (currentSortType) => {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+      <span class="trip-sort__item  trip-sort__item--day">${currentSortType === SortType.EVENT ? `Day` : ``}</span>
       ${createTripSortItemTemplate(currentSortType)}
       <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
     </form>`
